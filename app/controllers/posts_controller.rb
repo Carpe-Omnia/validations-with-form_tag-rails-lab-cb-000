3fd69d2ca0069f93aa2a2a1203ed1a57cad016cc
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-30 18:56:23 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,10 +23,15 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post.content = params[:content]
+    @post.title = params[:title]
+    @post.category = params[:category]
+    if @post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edut
+    end
   end
 
   private
